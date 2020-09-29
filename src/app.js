5b7f2c62059663ab50a7c2a5ccc00560951a6145
bash <wildandawambash@gmail.com>
2020-09-29 08:35:43 +0700

    fix subtotal if increment and decrement qty

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 import { 
   incrementQty, 
-  decrementQty
+  decrementQty,
+  subTotal
 } from './helpers.js';
 
 const incrButton = document.querySelector('#incr');
 const decrButton = document.querySelector('#decr');
 const qtyInput = document.querySelector('#qty');
+const textSubTotal = document.querySelector('#subtotal');
+const priceInput = document.querySelector("#price");
 
 incrButton.addEventListener('click', () => {
   qtyInput.value = incrementQty(qtyInput.value);
+  textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value)}`;
 });
 
 decrButton.addEventListener('click', () => {
   if(parseInt(qtyInput.value) > 1){
     qtyInput.value = decrementQty(qtyInput.value);  
+    textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value)}`;
   }
 });
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,9 @@
 const incrementQty = (qty) => parseInt(qty) + 1;
 const decrementQty = (qty) => parseInt(qty) - 1;
+const subTotal = (price, qty) => parseInt(price) * parseInt(qty);
 
 module.exports = { 
     incrementQty, 
-    decrementQty
+    decrementQty,
+    subTotal
 };
